ef08e50364679f9b1668844d4e21d1aba492f3f5
Henrique <henriqueferreira.fox@gmail.com>
2022-09-06 14:54:07 -0300

    Remove transfer-html and README.md

--- transfer-html/js/scrollController.ts
+++
@@ -1,9 +0,0 @@
-const navmenu = document.querySelector("#nav-menu");
-
-document.addEventListener("scroll", (): void => {
-    const { scrollTop } = document.documentElement;
-    if(scrollTop >= 100) {
-        return navmenu.classList.add("solid");
-    }
-    navmenu.classList.remove("solid");
-});
